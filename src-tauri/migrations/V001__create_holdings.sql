088ba6eb6e5021557f476d86d49709f484294c74
brufordev <140243966+brufordev@users.noreply.github.com>
2026-04-21 16:28:33 +0100

    delete migrations

--- src-tauri/migrations/V001__create_holdings.sql
+++ src-tauri/migrations/V001__create_holdings.sql
@@ -1,11 +1,5 @@
-CREATE TABLE IF NOT EXISTS holdings (
+CREATE TABLE test
+(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    uuid TEXT NOT NULL UNIQUE,
-    amount_btc REAL NOT NULL,
-    purchase_price REAL,
-    purchase_date TEXT,
-    notes TEXT,
-    created_at TEXT NOT NULL DEFAULT (datetime('now')),
-    updated_at TEXT NOT NULL DEFAULT (datetime('now'))
 );
 
--- src-tauri/migrations/V003__create_tracked_xpubs.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS tracked_xpubs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    uuid TEXT NOT NULL UNIQUE,
-    label TEXT NOT NULL,
-    xpub TEXT NOT NULL UNIQUE,
-    created_at TEXT NOT NULL DEFAULT (datetime('now')),
-    updated_at TEXT NOT NULL DEFAULT (datetime('now'))
-);
-
