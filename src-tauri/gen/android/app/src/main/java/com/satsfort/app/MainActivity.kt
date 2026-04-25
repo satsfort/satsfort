5bf15176c19b979c97a21aba32952023a02b0b38
brufordev <140243966+brufordev@users.noreply.github.com>
2026-04-25 16:19:15 +0100

    fix android status bar color

--- src-tauri/gen/android/app/src/main/java/com/satsfort/app/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/satsfort/app/MainActivity.kt
@@ -2,10 +2,14 @@ package com.satsfort.app
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+    val controller = WindowCompat.getInsetsController(window, window.decorView)
+    controller.isAppearanceLightStatusBars = false
+    controller.isAppearanceLightNavigationBars = false
   }
 }
